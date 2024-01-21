18f3b0b07de113bb63a5c27cbc40532c66924af0
dawndie <voluongbang20@gmail.com>
2024-01-21 12:02:57 +0900

    #11 container with most water

--- 11_container_with_most_water.go
+++ 11_container_with_most_water.go
@@ -1,6 +1,17 @@
 package main
 
 func maxArea(height []int) int {
-	
-	return 5
+	left, right := 0, len(height)-1
+	maxArea := 0
+	for left < right {
+		if area := (min(height[left], height[right]) * (right - left)); area > maxArea {
+			maxArea = area
+		}
+		if height[left] > height[right] {
+			right--
+		} else {
+			left++
+		}
+	}
+	return maxArea
 }
--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print(isSubsequence("fk", "adefbfdfc"))
+	fmt.Print(maxArea([]int{1}))
 }
