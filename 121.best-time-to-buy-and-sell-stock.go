c0780d3ec9ff3551c1c7b110a756ec6cf0083b8b
dawndie <voluongbang20@gmail.com>
2024-02-20 16:07:00 +0900

    #121

---
+++ 121.best-time-to-buy-and-sell-stock.go
@@ -0,0 +1,25 @@
+package main
+
+/*
+ * @lc app=leetcode id=121 lang=golang
+ *
+ * [121] Best Time to Buy and Sell Stock
+ */
+
+// @lc code=start
+func maxProfit(prices []int) int {
+	maxProfit := 0
+	lowestPrice := prices[0]
+	for _, price := range prices {
+		if price < lowestPrice {
+			lowestPrice = price
+		} else {
+			if currentProfit := price - lowestPrice; currentProfit > maxProfit {
+				maxProfit = currentProfit
+			}
+		}
+	}
+	return maxProfit
+}
+
+// @lc code=end
