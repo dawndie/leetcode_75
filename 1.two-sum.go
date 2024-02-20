6066dbeb22910f0a19afe9132a951f2f663ba089
dawndie <voluongbang20@gmail.com>
2024-02-20 10:37:29 +0900

    #1 two sum

---
+++ 1.two-sum.go
@@ -0,0 +1,22 @@
+package main
+
+/*
+ * @lc app=leetcode id=1 lang=golang
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+func twoSum(nums []int, target int) []int {
+	hashMap := make(map[int]int)
+	for position, element := range nums {
+		if _, ok := hashMap[element]; ok {
+			return []int{hashMap[element], position}
+		}
+		complement := target - element
+		hashMap[complement] = position
+	}
+	return []int{0, 0}
+}
+
+// @lc code=end
