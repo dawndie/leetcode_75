44ed43b4497140815d511cf3b9f9a6cc4564e037
dawndie <voluongbang20@gmail.com>
2024-02-24 16:59:52 +0900

    #141 cycle linked list

---
+++ 110.balanced-binary-tree.go
@@ -0,0 +1,43 @@
+package main
+
+/*
+ * @lc app=leetcode id=110 lang=golang
+ *
+ * [110] Balanced Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isBalanced(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	leftHeight := maxHeight(root.Left)
+	rightHeight := maxHeight(root.Right)
+	if leftHeight-rightHeight > 1 || leftHeight-rightHeight < -1 {
+		return false
+	}
+	return isBalanced(root.Left) == true && isBalanced(root.Right) == true
+}
+
+func maxHeight(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	leftHeight := maxHeight(root.Left) + 1
+	rightHeight := maxHeight(root.Right) + 1
+	if leftHeight > rightHeight {
+		return leftHeight
+	} else {
+		return rightHeight
+	}
+}
+
+// @lc code=end
