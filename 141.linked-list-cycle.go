92c039488c67db812ac9e31a8a20b6073e133488
dawndie <voluongbang20@gmail.com>
2024-02-24 17:00:13 +0900

    #141

---
+++ 141.linked-list-cycle.go
@@ -0,0 +1,32 @@
+package main
+
+/*
+ * @lc app=leetcode id=141 lang=golang
+ *
+ * [141] Linked List Cycle
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func hasCycle(head *ListNode) bool {
+	linkedListMap := make(map[*ListNode]bool)
+	pointer := head
+	for pointer != nil {
+		if linkedListMap[pointer] == false {
+			linkedListMap[pointer] = true
+			pointer = pointer.Next
+		} else {
+			return true
+		}
+	}
+	return false
+}
+
+// @lc code=end
